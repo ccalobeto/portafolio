774e065692cfbcc8ddcaee5bef705b36859dfb52
ccalobeto <calobeto.leon@gmail.com>
2024-11-15 21:34:42 -0500

    It fixes #2 silence css warning at build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		minify: false,
+		terserOptions: {
+			compress: false,
+			mangle: false
+		}
+	}
 }
 
 export default config
