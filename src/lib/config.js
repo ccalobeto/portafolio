0402f0f50a14cee323704f3a1db5ec5685e34a9d
ccalobeto <calobeto.leon@gmail.com>
2024-11-18 10:00:45 -0500

    add project config file

---
+++ src/lib/config.js
@@ -0,0 +1,53 @@
+/**
+ * All of these values are used throughout the site – for example, 
+ * in the <meta> tags, in the footer, and in the RSS feed.
+ * 
+ * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
+ **/ 
+export const metaImagesUrl ={
+  ogImage: 'https://joshcollinsworth.com/images/site-image.png',
+  twitterImage: 'https://joshcollinsworth.com/images/site-image.png'
+}
+export const siteTitles = {
+  aboutMe: 'Carlos León | Acerca de mi',
+  contact: 'Carlos León | Contacto',
+  utilities: 'Carlos León | Utilidades',
+  projects: 'Carlos León | Proyectos',
+  blog: 'Carlos León | Blog',
+  home: 'Carlos León',
+  category: 'Carlos León | Categoría',
+  allCategories: 'Carlos León | Categorías',
+}
+export const siteURL = 'https://carlosleon.dev'
+export const siteAuthor = '- find and change this text in src/lib/config.js'
+
+// Controls how many posts are shown per page on the main blog index pages
+export const postsPerPage = 10
+
+// Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
+export const navItems = {
+  home: {
+    title: 'Inicio',
+    route: '/'
+  },
+	blog:{
+		title: 'Blog',
+		route: '/blog'
+	}, 
+  projects:{
+    title: 'Proyectos',
+    route: '/projects'
+  }, 
+  about:{
+		title: 'Acerca de mi',
+		route: '/about-me'
+	}, 
+  contact:{
+		title: 'Contacto',
+		route: '/contact' 
+	}, 
+  uses:{
+    title: 'Utilidades',
+    route: '/uses'
+  }
+}
